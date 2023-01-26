6fffe46eda2ff206285037fa8b858bd91f624830
ARKEIN17 <ramirez1kevin98@gmail.com>
2023-01-26 15:20:31 -0500

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,5 +5,12 @@
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+        Args:
+            str1: The first string parameter
+            str1: The second string parameter
+        Return:
+            Concatenated string
+    """
 
     return (str1 + str2)
