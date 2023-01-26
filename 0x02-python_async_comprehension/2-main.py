35eb402bacdcff1f366bea650d05f7ea8c2db82c
ARKEIN17 <ramirez1kevin98@gmail.com>
2023-01-25 22:58:00 -0500

    task 2

---
+++ 0x02-python_async_comprehension/2-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+
+measure_runtime = __import__('2-measure_runtime').measure_runtime
+
+
+async def main():
+    return await(measure_runtime())
+
+print(
+    asyncio.run(main())
+)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Term
