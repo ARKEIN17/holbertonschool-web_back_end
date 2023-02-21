aa6198867f1fe04e152d3d3a8eb47a6e156e7a14
ARKEIN17 <ramirez1kevin98@gmail.com>
2023-02-21 03:27:23 -0500

    task 9

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -68,15 +68,14 @@ class Auth:
             return user
 
     def destroy_session(self, user_id: int) -> None:
-        """ updates the corresponding user’s session ID to None """
+        """ updates """
         try:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
 
     def get_reset_password_token(self, email: str) -> str:
-        """ If it exists, generate a UUID and update the user’s reset_token
-            database field. Return the token """
+        """  """
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
